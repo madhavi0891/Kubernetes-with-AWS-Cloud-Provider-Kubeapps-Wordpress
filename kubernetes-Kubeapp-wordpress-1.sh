f78ccffc22e2485d80b9bb893d841a35dc3f774b
madhavi0891 <66946576+madhavi0891@users.noreply.github.com>
2020-09-16 07:49:00 +0530

    Update kubernetes-Kubeapp-wordpress-1.sh

--- kubernetes-Kubeapp-wordpress-1.sh
+++ kubernetes-Kubeapp-wordpress-1.sh
@@ -62,7 +62,7 @@ cat > /etc/docker/daemon.json <<EOF
   "exec-opts": ["native.cgroupdriver=systemd"],
   "log-driver": "json-file",
   "log-opts": {
-    "max-size": "100m"
+    "max-size": "1000m"
   },
   "storage-driver": "overlay2"
 }
